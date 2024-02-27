f4cad3e2c7140aff293c266549fb491fd116eba8
bhavsaryash06 <118473382+bhavsaryash06@users.noreply.github.com>
2024-02-27 16:21:49 -0500

    Update MusicDatabasesqlQuery.sql

--- Music Database/MusicDatabasesqlQuery.sql
+++ Music Database/MusicDatabasesqlQuery.sql
@@ -101,10 +101,6 @@ WHERE miliseconds > (
 ORDER BY miliseconds DESC;
 
 
-
-
-/* Question Set 3 - Advance */
-
 /* Q1: Find how much amount spent by each customer on artists? Write a query to return customer name, artist name and total spent */
 
 /* Steps to Solve: First, find which artist has earned the most according to the InvoiceLines. Now use this artist to find 
@@ -156,31 +152,6 @@ WITH popular_genre AS
 )
 SELECT * FROM popular_genre WHERE RowNo <= 1
 
-
-/* Method 2: : Using Recursive */
-
-WITH RECURSIVE
-	sales_per_country AS(
-		SELECT COUNT(*) AS purchases_per_genre, customer.country, genre.name, genre.genre_id
-		FROM invoice_line
-		JOIN invoice ON invoice.invoice_id = invoice_line.invoice_id
-		JOIN customer ON customer.customer_id = invoice.customer_id
-		JOIN track ON track.track_id = invoice_line.track_id
-		JOIN genre ON genre.genre_id = track.genre_id
-		GROUP BY 2,3,4
-		ORDER BY 2
-	),
-	max_genre_per_country AS (SELECT MAX(purchases_per_genre) AS max_genre_number, country
-		FROM sales_per_country
-		GROUP BY 2
-		ORDER BY 2)
-
-SELECT sales_per_country.* 
-FROM sales_per_country
-JOIN max_genre_per_country ON sales_per_country.country = max_genre_per_country.country
-WHERE sales_per_country.purchases_per_genre = max_genre_per_country.max_genre_number;
-
-
 /* Q3: Write a query that determines the customer that has spent the most on music for each country. 
 Write a query that returns the country along with the top customer and how much they spent. 
 For countries where the top amount spent is shared, provide all customers who spent this amount. */
@@ -200,24 +171,3 @@ WITH Customter_with_country AS (
 SELECT * FROM Customter_with_country WHERE RowNo <= 1
 
 
-/* Method 2: Using Recursive */
-
-WITH RECURSIVE 
-	customter_with_country AS (
-		SELECT customer.customer_id,first_name,last_name,billing_country,SUM(total) AS total_spending
-		FROM invoice
-		JOIN customer ON customer.customer_id = invoice.customer_id
-		GROUP BY 1,2,3,4
-		ORDER BY 2,3 DESC),
-
-	country_max_spending AS(
-		SELECT billing_country,MAX(total_spending) AS max_spending
-		FROM customter_with_country
-		GROUP BY billing_country)
-
-SELECT cc.billing_country, cc.total_spending, cc.first_name, cc.last_name, cc.customer_id
-FROM customter_with_country cc
-JOIN country_max_spending ms
-ON cc.billing_country = ms.billing_country
-WHERE cc.total_spending = ms.max_spending
-ORDER BY 1;
